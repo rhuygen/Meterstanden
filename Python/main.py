da3d4361cc9300c43d50508e0afacfdacef9b71f
Rik Huygen <rik.huygen@kuleuven.be>
2018-01-27 07:28:51 +0100

    Use the QDarkStyle

--- Python/main.py
+++ Python/main.py
@@ -2,13 +2,17 @@
 
 import sys, os, logging
 import numpy as np
+import qdarkstyle
 
 from PyQt5 import QtWidgets
+
 from main_window import Ui_MainWindow
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(message)s")
 
 app = QtWidgets.QApplication(sys.argv)
+
+app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
 
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_MainWindow()
