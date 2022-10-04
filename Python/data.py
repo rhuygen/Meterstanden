e8f4f790abe39a9279d43492cc6a29b41f80be56
Rik Huygen <rik.huygen@kuleuven.be>
2022-10-04 10:15:44 +0200

    Daily updates

--- Python/data.py
+++ Python/data.py
@@ -10,7 +10,7 @@ HOME_DIR = os.path.expanduser('~')
 def load_data(logger=None):
     # Read the input csv file
     input_filename = 'Private/Git/Meterstanden/Data/Meterstanden.csv'
-    
+
     logger.info("Reading input csv file from {}.".format(input_filename))
 
     data = np.recfromcsv(os.path.join(HOME_DIR, input_filename))
@@ -19,8 +19,7 @@ def load_data(logger=None):
     df = pd.read_csv(os.path.join(HOME_DIR, input_filename), index_col="Date_Time", parse_dates=True)
 
     # Convert the byte string b'' into a 'normal' string
-    
+
     dt = np.array([datetime.datetime.strptime(x.decode('UTF-8'), '%Y-%m-%d %H:%M') for x in data['date_time']])
 
-    return (data, df, dt)
-
+    return data, df, dt
--- Python/main.py
+++ Python/main.py
@@ -26,10 +26,10 @@ yearsFmt = mdates.DateFormatter('%Y')
 
 @logme.log
 class MainWindow_EXEC():
-            
+
     def __init__(self):
         app = QtWidgets.QApplication(sys.argv)
-        
+
         # 'dark' theme will be used for matplotlib when creating the figure
         self.theme = "dark"
 
@@ -37,17 +37,17 @@ class MainWindow_EXEC():
         app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
 
         app.setApplicationDisplayName('Meterstanden')
-        
+
         self.MainWindow = QtWidgets.QMainWindow()
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(self.MainWindow)  
+        self.ui.setupUi(self.MainWindow)
 
         self.MainWindow.show()
 
         self.init_tabs()
 
-        sys.exit(app.exec_()) 
-        
+        sys.exit(app.exec_())
+
     #---------------------------------------------------------------------------
     def init_tabs(self):
         import data
@@ -105,60 +105,60 @@ class MainWindow_EXEC():
 
         #fig = plt.figure(figsize=(FIGSIZE_X/DPI, FIGSIZE_Y/DPI), dpi=DPI)
         #fig.suptitle("Meterstanden")
-        
+
         title = "Verbruik Water"
-        
+
         self.logger.debug(f"Creating GUI for {title}")
 
         canvas.axes.cla()
         canvas.setColorScheme(self.theme)
 
         canvas.axes.set_title(title)
-        
+
         canvas.axes.plot(self.dt, self.data['water'], '-')
         canvas.axes.plot(self.dt, self.data['water'], '.')
-        
+
         canvas.axes.set_xlabel("Datum")
         canvas.axes.set_ylabel("Volume [m$^3$]")
-        
+
         # format the ticks
         canvas.axes.xaxis.set_major_locator(years)
         canvas.axes.xaxis.set_major_formatter(yearsFmt)
         canvas.axes.xaxis.set_minor_locator(months)
-        
+
         #datemin = datetime.date(dt.min().year, 1, 1)
         #datemax = datetime.date(dt.max().year + 1, 1, 1)
         #self.axes.set_xlim(datemin, datemax)
-        
+
         canvas.axes.format_xdata = mdates.DateFormatter('%Y-%m-%d')
         canvas.axes.grid(True)
-        
+
         # Tell matplotlib to interpret the x-axis values as dates
-        
+
         #self.axes.xaxis_date()
-        
+
         # Create a 5% (0.05) and 10% (0.1) padding in the
         # x and y directions respectively.
         #plt.margins(0.05, 0.1)
-        
+
         # Make space for and rotate the x-axis tick labels
-        
+
         #fig.autofmt_xdate()
-        
+
         #plt.show()
         #plt.close()
-        
+
         canvas.draw()
 
         pass
 
 
     def drawGasVerbruik(self, canvas):
-        
+
         # Tell matplotlib to interpret the x-axis values as dates
-        
+
         title = "Verbruik Gas"
-        
+
         self.logger.debug(f"Creating GUI for {title}")
 
         canvas.axes.cla()
@@ -169,23 +169,23 @@ class MainWindow_EXEC():
 
         canvas.axes.plot(self.dt, self.data['gas'], '-')
         canvas.axes.plot(self.dt, self.data['gas'], '.')
-        
+
         canvas.axes.set_xlabel("Datum")
         canvas.axes.set_ylabel("Verbruik Gas [m$^3$]")
-        
+
         canvas.axes.format_xdata = mdates.DateFormatter('%Y-%m-%d')
         canvas.axes.grid(True)
-        
+
         #plt.setp(canvas.axes.xaxis.get_majorticklabels(), rotation=30)
 
         #canvas.axes.get_xmajorticklabels().set_rotation(30)
-        
+
         # Create a 5% (0.05) and 10% (0.1) padding in the
         # x and y directions respectively.
         #plt.margins(0.05, 0.1)
-        
+
         # Make space for and rotate the x-axis tick labels
-        
+
         #fig.autofmt_xdate()
 
         canvas.draw()
@@ -198,16 +198,16 @@ class MainWindow_EXEC():
         self.logger.debug(f"Creating GUI for {title}")
 
         # Bereken het verbruik in gas tussen twee opeenvolgende metingen
-        
+
         g_diff = self.df['Gas'].diff().dropna()
-        
+
         # Bereken de tijd tussen twee opeenvolgende metingen in dagen (both options are the same)
-        
+
         time_diff_days = g_diff.index.to_series().diff().astype('timedelta64[s]') / 60. / 60. / 24.
         time_diff_days = g_diff.index.to_series().diff().dt.total_seconds().fillna(0) / 60. / 60. / 24.
-        
+
         # Bereken het verbruik van gas per dag
-        
+
         g_per_dag = g_diff / time_diff_days
 
         canvas.axes.cla()
@@ -246,7 +246,7 @@ class MainWindow_EXEC():
         canvas.axes.plot(self.dt, e_total)
         canvas.axes.plot(self.dt, e_total, '.')
         canvas.axes.grid(True)
-        
+
         plt.margins(0.05, 0.1)
 
         canvas.draw()
@@ -260,27 +260,27 @@ class MainWindow_EXEC():
 
         # Bereken het verbruik in elektriciteit tussen twee opeenvolgende metingen
         # Elektriciteit voor de dag en nacht teller eerst bijeen tellen
-        
+
         e_total = self.df.eDag + self.df.eNacht
         e_diff = e_total.diff().dropna()
-        
+
         # Bereken de tijd tussen twee opeenvolgende metingen in dagen
-        
+
         time_diff_days = e_diff.index.to_series().diff().dt.total_seconds().fillna(0) / 60. / 60. / 24.
-        
+
         # Bereken het elektriciteitsverbruik per dag
-        
+
         e_per_dag = e_diff / time_diff_days
-        
+
         # Bereken de totale opbrengst per dag van de zonnepanelen
-        
+
         z_total = self.df.SMA_3000.shift(1) + self.df.SMA_7000.shift(1)
         z_total = z_total.dropna()
-        
+
         self.logger.debug(f"Totale opbrengst zonnepanelen op {z_total.index[-1]}: {z_total[-1]} [kWh]")
-        
+
         # Het totaal verbruik per dag is de sum van het elektriciteitsverbruik en de opbrengst van de zonnepanelen
-        
+
         ez_per_dag = e_per_dag + z_total
 
 
@@ -302,9 +302,9 @@ class MainWindow_EXEC():
 
     def drawZonnepanelen(self, canvas):
 
-        # De SMA 7000 bevat 32 zonnepanelen, de SMA 3000 bevat er 14. 
+        # De SMA 7000 bevat 32 zonnepanelen, de SMA 3000 bevat er 14.
         # De verhouding zou dus ± 2.28 moeten zijn
-        
+
         sma_ratio = self.df.SMA_7000 / self.df.SMA_3000
 
         title = "Zonnepanelen"
@@ -316,13 +316,13 @@ class MainWindow_EXEC():
         canvas.setColorScheme(self.theme)
 
         canvas.axes.set_title(title)
-        
-        canvas.axes.xaxis_date()        
+
+        canvas.axes.xaxis_date()
         canvas.axes.set_xlabel("Datum")
         canvas.axes.set_ylabel("SMA 7000 / SMA 3000")
         canvas.axes.plot(sma_ratio, ".")
         canvas.axes.grid(True)
-        
+
         plt.margins(0.05, 0.1)
 
         canvas.draw()
