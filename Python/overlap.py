18eaf6f526985db9bc38c5c84575ba4cd54d159e
Rik Huygen <rik.huygen@kuleuven.be>
2022-10-14 11:40:49 +0200

    Daily updates + improved plotting in overlap.py

--- Python/overlap.py
+++ Python/overlap.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as md
@@ -32,7 +34,7 @@ piv[("Gas", 2020)] -= 10500.
 piv[("Gas", 2021)] -= 14032.494
 piv[("Gas", 2022)] -= 17744.537
 
-piv[[("Gas", 2016), ("Gas", 2017), ("Gas", 2018), ("Gas", 2019), ("Gas", 2020), ("Gas", 2021), ("Gas", 2022)]].plot()
+# piv[[("Gas", 2016), ("Gas", 2017), ("Gas", 2018), ("Gas", 2019), ("Gas", 2020), ("Gas", 2021), ("Gas", 2022)]].plot()
 
 
 # piv.plot()
@@ -40,20 +42,43 @@ piv[[("Gas", 2016), ("Gas", 2017), ("Gas", 2018), ("Gas", 2019), ("Gas", 2020), 
 
 # offset for day of year
 
-offset_280 = {2015: 0.288, 2016: 1664.936, 2017: 3936.008, 2019: 8700.027, 2022: 19096.456}  # 7 oct
-offset_226 = {2016: 1587.984, 2017: 3846.380, 2018: 5533.090, 2019: 8453.316, 2022: 19032.746}  # 14 aug
+doy_280 = datetime.datetime(2022, 10, 7, 0, 0, 0, 0).strftime("%j")
+doy_226 = datetime.datetime(2022, 8, 14, 0, 0, 0, 0).strftime("%j")
+doy_027 = datetime.datetime(2022, 1, 27, 0, 0, 0, 0).strftime("%j")
 
-offsets = offset_226
+# -------------------- Make the plot for Gas ---------------------------------------------------------------------------
 
+offset_gas_280 = {2015: 0.288, 2016: 1664.936, 2017: 3936.008, 2019: 8700.027, 2022: 19096.456}
+offset_gas_226 = {2016: 1587.984, 2017: 3846.380, 2018: 5533.090, 2019: 8453.316, 2022: 19032.746}
+offset_gas_027 = {2016: 688, 2017: 2780, 2018: 4700, 2019: 7220, 2020: 10500, 2021: 14032, 2022: 17745}
+offsets = offset_gas_027
 
-# fig, ax = plt.subplots(figsize=(16, 8))
-#
-# for year in offsets:  #2015, 2016, 2017, 2019, 2022:  # df['year'].unique():
-#     x = df[df['year'] == year]['doy']
-#     y = df[df['year'] == year]['Gas']
-#     y = df[df['year'] == year]['eDag']
-    # ax.plot(x, y-offsets[year], label=year)
-    # ax.plot(x, y, label=year)
+fig, (ax_gas, ax_edag) = plt.subplots(2, 1, sharex=True, figsize=(16, 16))
+
+for year in offsets:
+    x = df[df['year'] == year]['doy']
+    y = df[df['year'] == year]['Gas']
+    ax_gas.scatter(x, y-offsets[year], label=year)
+
+ax_gas.set_ylabel("m$^3$")
+ax_gas.set_title("Jaarlijks Gasverbruik")
+ax_gas.legend()
+
+# -------------------- Make the plot for electricity -------------------------------------------------------------------
+
+offset_edag_027 = {2016: 164082, 2017: 167001, 2018: 169195, 2019: 168437, 2020: 168600, 2021: 168812, 2022: 170393}
+offsets = offset_edag_027
+
+for year in offsets:
+    x = df[df['year'] == year]['doy']
+    y = df[df['year'] == year]['eDag']
+    ax_edag.scatter(x, y-offsets[year], label=year)
+
+ax_edag.set_ylabel("kWh")
+ax_edag.set_title("Jaarlijks Elektriciteitsverbruik")
+ax_edag.legend()
+
+plt.show()
 
 # palette = sns.color_palette('Set1', n_colors = len(df['year'].unique()))
 # sns.lineplot(
@@ -68,5 +93,3 @@ offsets = offset_226
 # ax.xaxis.set_major_locator(md.DMonthLocator())
 # ax.xaxis.set_major_formatter(md.DateFormatter('%b'))
 # ax.set_xlim([df['month'].iloc[0], df['month'].iloc[-1]])
-# plt.legend()
-# plt.show()
