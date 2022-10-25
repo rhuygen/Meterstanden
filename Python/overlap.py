a6ede2ac0692191a395e7301dde953166b1ffe02
Rik Huygen <rik.huygen@kuleuven.be>
2022-10-25 12:37:54 +0200

    proper offset 3rd of January

--- Python/overlap.py
+++ Python/overlap.py
@@ -51,7 +51,9 @@ doy_027 = datetime.datetime(2022, 1, 27, 0, 0, 0, 0).strftime("%j")
 offset_gas_280 = {2015: 0.288, 2016: 1664.936, 2017: 3936.008, 2019: 8700.027, 2022: 19096.456}
 offset_gas_226 = {2016: 1587.984, 2017: 3846.380, 2018: 5533.090, 2019: 8453.316, 2022: 19032.746}
 offset_gas_027 = {2016: 688, 2017: 2780, 2018: 4700, 2019: 7220, 2020: 10500, 2021: 14032, 2022: 17745}
-offsets = offset_gas_027
+offset_gas_003 = {2016: 499, 2017: 2438, 2018: 4535, 2019: 6744, 2020: 10102, 2021: 13511, 2022: 17247}
+
+offsets = offset_gas_003
 
 # 110.x DPI -> 1440 Pixels = 13"
 # 130.x DPI -> 1680 Pixels = 13"
@@ -62,7 +64,7 @@ fig, (ax_gas, ax_edag) = plt.subplots(2, 1, sharex=True, figsize=(7, 7), dpi=130
 for year in offsets:
     x = df[df['year'] == year]['doy']
     y = df[df['year'] == year]['Gas']
-    ax_gas.scatter(x, y-offsets[year], label=year)
+    ax_gas.scatter(x, y-offsets[year], label=year, s=6)
 
 ax_gas.set_ylabel("m$^3$")
 ax_gas.set_title("Jaarlijks Gasverbruik")
@@ -71,12 +73,14 @@ ax_gas.legend()
 # -------------------- Make the plot for electricity -------------------------------------------------------------------
 
 offset_edag_027 = {2016: 164082, 2017: 167001, 2018: 169195, 2019: 168437, 2020: 168600, 2021: 168812, 2022: 170393}
-offsets = offset_edag_027
+offset_edag_003 = {2016: 163695, 2017: 166632, 2018: 168820, 2019: 168041, 2020: 168317, 2021: 168206, 2022: 169785}
+
+offsets = offset_edag_003
 
 for year in offsets:
     x = df[df['year'] == year]['doy']
     y = df[df['year'] == year]['eDag']
-    ax_edag.scatter(x, y-offsets[year], label=year)
+    ax_edag.scatter(x, y-offsets[year], label=year, s=6)
 
 ax_edag.set_ylabel("kWh")
 ax_edag.set_title("Jaarlijks Elektriciteitsverbruik")
